b188c2580a2f405b0ba1239a56974bdbd0d5e7aa
ExposedCat <artem13.prokop@gmail.com>
2021-09-21 23:24:38 +0300

    Add all commands to client app

--- client/src/server.js
+++ client/src/server.js
@@ -18,6 +18,64 @@ function setupServer(port) {
         }
         if (query) {
             await matchByRegex(query, [
+                [
+                    /^Who is head of department (.+)$/,
+                    async name => {
+                        const { error, data } = await localApiCall(
+                            'getHeadOfDepartment', { name }
+                        )
+                        if (error === null) {
+                            result.message = `Head of ${name} department is ${data.data}`
+                        }
+                    }
+                ],
+                [
+                    /^Show (.+) statistic$/,
+                    async name => {
+                        const { error, data } = await localApiCall(
+                            'getDepartmentStatistic', { name }
+                        )
+                        if (error === null) {
+                            const { assistants, associateProfessors, professors } = data.data
+                            result.message = `Assistants: ${assistants}\n`
+                            result.message += `Associate professors: ${associateProfessors}\n`
+                            result.message += `Professors: ${professors}`
+                        }
+                    }
+                ],
+                [
+                    /^Show the average salary for department (.+)$/,
+                    async name => {
+                        const { error, data } = await localApiCall(
+                            'getAverageSalaryOfDepartment', { name }
+                        )
+                        if (error === null) {
+                            result.message = data.data
+                        }
+                    }
+                ],
+                [
+                    /^Show count of employee for (.+)$/,
+                    async name => {
+                        const { error, data } = await localApiCall(
+                            'getCountOfEmployeeForDepartment', { name }
+                        )
+                        if (error === null) {
+                            result.message = data.data
+                        }
+                    }
+                ],
+                [
+                    /^Global search by (.+)$/,
+                    async query => {
+                        const { error, data } = await localApiCall(
+                            'globalSearch', { query }
+                        )
+                        if (error === null) {
+                            result.message = data.data
+                        }
+                    }
+                ]
             ])
         }
         response.send(result)
